1db1d4b9718f4948d407adc44f6712debfe956d4
Ashok <ashoknath@Ashoks-MacBook-Pro.local>
2021-06-13 22:42:06 +1000

    Creating a protectd route with token access

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use \App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,13 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class,'store']);
 
-Route::resource('products', ProductController::class);
+//Route::resource('products', ProductController::class);
+
+
+//Public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class,'search']);
 
+//Protected routes 
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class,'store']);
+    Route::put('/products/{id}', [ProductController::class,'update']);
+    Route::delete('/products/{id}', [ProductController::class,'destroy']); 
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
