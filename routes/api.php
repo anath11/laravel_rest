1e5ee68f897104927c855b3d776f1743e2557035
Ashok <ashoknath@Ashoks-MacBook-Pro.local>
2021-06-13 23:26:27 +1000

    Created full rest api laravel app

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,15 +20,20 @@ use \App\Http\Controllers\ProductController;
 
 
 //Public routes
+Route::post('/register', [AuthController::class,'register']);
+Route::post('/login', [AuthController::class,'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class,'search']);
+
+
 
 //Protected routes 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/products', [ProductController::class,'store']);
     Route::put('/products/{id}', [ProductController::class,'update']);
     Route::delete('/products/{id}', [ProductController::class,'destroy']); 
+    Route::post('/logout', [AuthController::class,'logout']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
